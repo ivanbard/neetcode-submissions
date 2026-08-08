64abb60155c7aa6808fb50ea316e8895f115233d
Ivan Bardziyan <77278718+ivanbard@users.noreply.github.com>
2026-08-08 15:35:04 -0400

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # cycle = any nodes next pointer being a previous node
+        # hash table to track seen nodes
+        # view what nodes route back to prev node and save in hash map?
+        seen = {}
+        #prev = None
+        curr = head
+
+        while curr:
+            # save next address for comparisons
+            if curr.next in seen:
+                return True
+            else:
+                seen[curr] = curr.next
+
+            #prev = curr
+            curr = curr.next
+
+        return False
